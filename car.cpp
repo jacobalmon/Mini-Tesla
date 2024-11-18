22325864af9432e2f0c6048b0bfe068044ad5f95
Jacob Almon <118553065+jacobalmon@users.noreply.github.com>
2024-11-18 10:09:09 -0800

    Update car.cpp

--- car.cpp
+++ car.cpp
@@ -1,5 +1,20 @@
 #include "mbed.h"
 
+// Define Pins for Hardware.
+
+// Constraints.
+
+// Global Varaibles.
+
+// Function Prototypes.
+void bluetooth_communication();
+void detect_obstacle();
+void movement_control();
+void send_alert();
+void power_management();
+
 int main() {
-  
+    while (1) {
+
+    }
 }
