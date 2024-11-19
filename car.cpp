7d39bca0b2ecf49fe500fcfe4e35f5414590f4ee
Jacob Almon <118553065+jacobalmon@users.noreply.github.com>
2024-11-18 19:49:58 -0800

    Update car.cpp

--- car.cpp
+++ car.cpp
@@ -1,10 +1,20 @@
 #include "mbed.h"
 
 // Define Pins for Hardware.
+DigitalIn ultrasonic_sensor_trigger();
+DigitalIn ultrasonic_sensor_echo();
+DigitalOut motor_forward();
+DigitalOut motor_backward();
+DigitalOut motor_left();
+DigitalOut motor_right();
+Serial bluetooth();
 
 // Constraints.
+const int OBSTACLE_THRESHOLD = 10;
 
 // Global Varaibles.
+bool obstacle_detected = false;
+string movement_command = "Stop";
 
 // Function Prototypes.
 void bluetooth_communication();
